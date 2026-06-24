c12ada1a9ad439c619caf59d1744f68e9400f704
user <user@DESKTOP-DVR4V9R>
2026-06-24 15:32:22 +0530

    commit

---
+++ Test Suites/Suite1.0.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite1.0</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>6cf0bd9c-33c2-49a0-86b6-4d32882e7270</testSuiteGuid>
+   <testCaseLink>
+      <guid>78640676-dea3-48fe-8cf2-e956f1b3d8e1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Manual Mode/ManualMode1.0</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
